582280f79ebe231eff96e1330663b052663eacec
unknown <abanoubsamir@gmail.com>
2021-12-31 19:57:49 +0200

    'news'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([LaratrustSeeder::class,UserTableSeederr::class]); 
-        // $this->call([TestimonialsSeeder::class,NewsSeeder::class]); 
-        // $this->call([MusicSeeder::class]); 
-        // $this->call([CommentSeeder::class]); 
+        $this->call([TestimonialsSeeder::class,NewsSeeder::class]); 
+        $this->call([MusicSeeder::class]); 
+        $this->call([CommentSeeder::class]); 
     }
 }
